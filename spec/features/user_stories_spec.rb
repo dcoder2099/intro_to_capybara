468e040f795677d0033dcf30558042a4887c5cdd
Daniel Hedrick <git@hedmail.com>
2013-01-19 15:58:30 -0600

    Convert 'buying a product' user story to RSpec/Capybara
    - This is mostly the same, except we don't perform some of the assertions
    that are hard/difficult/impossible to assert in a Capy test (like having
    access to response).
    - Also had to make one minor tweak to the 'Add to Cart' buttons to make
    it easier to select them in the tests.

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -11,7 +11,36 @@ describe 'user stories', type: :feature do
     visit "/"
     page.should have_content 'Your Pragmatic Catalog'
 
+    find("#product_#{ruby_book.id}").click
+
+    find('div#cart').should have_content 'Your Cart'
+    find('div#cart').should have_content ruby_book.title
+
+    find('div#cart input[value="Checkout"]').click
+    page.should have_content 'Please Enter Your Details'
+
+    fill_in 'Name', with: 'Dave Thomas'
+    fill_in 'Address', with: '123 The Street'
+    fill_in 'Email', with: 'dave@example.com'
+    select 'Check', from: 'Pay type'
+    find('input[value="Place Order"]').click
+    page.should have_content 'Thank you for your order'
+
     orders = Order.all
     orders.size.should eq(1)
+    order = orders[0]
+    order.name.should eq('Dave Thomas')
+    order.address.should eq('123 The Street')
+    order.email.should eq('dave@example.com')
+    order.pay_type.should eq('Check')
+
+    order.line_items.size.should eq(1)
+    line_item = order.line_items[0]
+    line_item.product.should eq(ruby_book)
+
+    mail = ActionMailer::Base.deliveries.last
+    mail.to.should include('dave@example.com')
+    mail[:from].value.should eq('Sam Ruby <depot@example.com>')
+    mail.subject.should eq('Pragmatic Store Order Confirmation')
   end
 end
